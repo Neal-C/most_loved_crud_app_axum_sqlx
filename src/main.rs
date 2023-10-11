58014f07e393528cb6b8bf4242897238b0b17bc3
Neal-C <pop6d531@duck.com>
2023-10-11 10:57:28 +0200

    add(basic port and address setup): setup main fn typing and set port + address

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use axum::http;
+use axum::routing::{get, Router};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let port = 3000;
+
+    let address: String = format!("0.0.0.0:{port}");
+
+    let app = Router::new();
+
+    axum::Server::bind(&address.parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+    Ok(())
 }
