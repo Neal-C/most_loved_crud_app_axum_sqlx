16203548c70b446aefd4098a496290e583dadadc
Neal-C <pop6d531@duck.com>
2023-10-12 13:17:27 +0200

    feat(DB-postgres):DB implementation -> created DB on fl0, installed sqlx-cli, ran migrations with the sqlx-cli

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 mod handlers;
 
 use axum::routing::{get, Router};
+use sqlx::postgres::PgPoolOptions;
+
+const MAX_PG_CONNECTIONS: u32 = 5;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,7 +11,17 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let address: String = format!("0.0.0.0:{port}");
 
-    let app = Router::new().route("/", get(handlers::heartbeat));
+    let database_url: String =
+        std::env::var("DATABASE_URL").expect("Missing DATABASE_URL environment variable");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(MAX_PG_CONNECTIONS)
+        .connect(&database_url)
+        .await?;
+
+    let app = Router::new()
+        .route("/", get(handlers::heartbeat))
+        .with_state(pool);
 
     axum::Server::bind(&address.parse().unwrap())
         .serve(app.into_make_service())
